c45bf9a37d8ef4dd598fc5635e14cbe230bb4b41
Computer Desktop <60677031+Classroom899@users.noreply.github.com>
2020-05-12 13:06:32 -0700

    Adding in my function to display current weather for a city

--- script.js
+++ script.js
@@ -9,4 +9,15 @@ var name = document.querySelector('.name');
 var description = document.querySelector('.description');
 var temp = document.querySelector('.temp');
 
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${APIKey}`)
+inputValue.addEventListener('click,', function () {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${APIKey}`)
+        .then(response => response.JSON())
+        .then(data => console.log(data)) // Data is stuff that we receive back from the API
+
+
+
+        .catch(err => alert('Wrong city name'))
+});
+
+
+
