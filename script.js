6cb20884de97a487741736f84e917addbd4dd25f
Computer Desktop <60677031+Classroom899@users.noreply.github.com>
2020-05-12 12:36:43 -0700

    Create the fetch for the city with api key already set

--- script.js
+++ script.js
@@ -5,3 +5,8 @@
 
 var inputValue = document.querySelector('.inputValue');
 var submitInput = document.querySelector('.submitInput');
+var name = document.querySelector('.name');
+var description = document.querySelector('.description');
+var temp = document.querySelector('.temp');
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${APIKey}`)
