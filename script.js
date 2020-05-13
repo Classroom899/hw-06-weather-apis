f831eb8dbaccfe97c969678590f5226068f08430
Computer Desktop <60677031+Classroom899@users.noreply.github.com>
2020-05-12 18:55:04 -0700

    Add jquery asking to have needed information displayed on the page correctly and append that information

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-// function searchCurrentWeather {
-
-
-// }
-
 var inputValue = document.querySelector('.inputValue');
 var submitInput = document.querySelector('.submitInput');
 var name = document.querySelector('.name');
 var description = document.querySelector('.description');
 var temp = document.querySelector('.temp');
 
-inputValue.addEventListener('click,', function () {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${APIKey}`)
+
+inputValue.addEventListener('click,', function display(currentWeather) {
+    (`https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${APIKey}`)
         .then(response => response.JSON())
-        .then(data => console.log(data)) // Data is stuff that we receive back from the API
+        .then(data => {
+            var nameValue = data['name'];
+            var tempValue = data['main']['temp'];
+            var descriptionValue = data['weather'][0]['description'];
+
+            name.innerHTML = nameValue;
+            description.innerHTML = descriptionValue;
+            temp.innerHTML = tempValue;
+
+
+        }) // Data is stuff that we receive back from the API
 
 
 
